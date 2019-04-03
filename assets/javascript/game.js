37fe17ed4fba6d09e43393730b0103874cfe3d10
nathan meyer <nmeyer@nathans-MacBook-Air.local>
2019-04-03 15:06:35 -0700

    COMMENT

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,32 @@
 
+    $(document).ready(function() {
+        $(this).mousemove (function(e) {
+          $("#light").css({
+            "top": e.pageY - 250,
+            "left": e.pageX - 250
+          })
+        }).mousedown (function(e) {
+          switch (e.which) {
+              case 1:
+                  $("#light").toggleClass("light-off");
+                  break;
+              case 2:
+                  console.log('Middle Mouse button pressed.');
+                  break;
+              case 3:
+                  console.log('Right Mouse button pressed.');
+                  break;
+              default:
+                  console.log('You have a strange Mouse!');
+    
+        }
+    })
+    })
+        
+        
+        
+        
+        
         // declare global variables
         var alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
         console.log(alphabetArray.length);
